9d070f42cd8ef8c59cf48d0a4f0002909ff88a9a
Shiroi <fabian_frontczak@wp.pl>
2020-05-01 00:13:50 +0200

    Added model classes for Presentation layer

---
+++ Presenation/Model/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presenation.Model
+{
+    public class Product
+    {
+        private int _id;
+        private string _name;
+        private string _description;
+        private string _type;
+        private string _unit;
+        private double _nettoPrice;
+        private double _vat;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Description { get => _description; set => _description = value; }
+        public string Type { get => _type; set => _type = value; }
+        public string Unit { get => _unit; set => _unit = value; }
+        public double NettoPrice { get => _nettoPrice; set => _nettoPrice = value; }
+        public double Vat { get => _vat; set => _vat = value; }
+
+        public Product()
+        {
+        }
+
+        public Product(int id, string name, string description, string type, string unit, double nettoPrice, double vat)
+        {
+            _id = id;
+            _name = name;
+            _description = description;
+            _type = type;
+            _unit = unit;
+            _nettoPrice = nettoPrice;
+            _vat = vat;
+        }
+    }
+}
