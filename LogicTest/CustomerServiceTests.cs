50f699d716379571343581ba1e4448864dbdbe48
Mantis1262 <Mateuszkarolak3@o2.pl>
2020-05-04 00:38:17 +0200

    Create project for logic tests class.

---
+++ LogicTest/CustomerServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicTest
+{
+    [TestClass]
+    public class CustomerServiceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ LogicTest/MerchandiseServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicTest
+{
+    [TestClass]
+    public class MerchandiseServiceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
